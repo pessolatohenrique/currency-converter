315730ad0199cdf1dd6c029c49102217ce9c5219
pessolatohenrique <pessolatohenrique@gmail.com>
2021-03-19 22:57:38 -0300

    Logica inicial para conversao de moeda

--- src/controllers/CurrencyController.js
+++ src/controllers/CurrencyController.js
@@ -2,13 +2,22 @@ const model = require("../models").Currency;
 const axios = require("axios");
 
 class CurrencyController {
-  static async index(req, res) {
+  static async index(req, res) {}
+
+  static async calculate(req, res) {
     try {
       const response = await axios.get(
         "https://api.exchangeratesapi.io/latest?base=BRL&symbols=USD,EUR,INR"
       );
 
-      return res.status(200).json(response.data);
+      const valuesCalcutated = Object.keys(response.data.rates).map((key) => {
+        const value = response.data.rates[key];
+        const resultCalculated = value < 1 ? 1 / value : 1 * value;
+
+        return { code: key, result: resultCalculated.toFixed(2) };
+      });
+
+      return res.status(200).json(valuesCalcutated);
     } catch (error) {
       return res.status(500).json(error);
     }
--- src/routes/currency.js
+++ src/routes/currency.js
@@ -4,6 +4,6 @@ const CurrencyController = require("../controllers/CurrencyController");
 const router = Router();
 
 router.get("/", (req, res) => res.status(200).send({ message: "initial" }));
-router.get("/currency", CurrencyController.index);
+router.get("/currency/calculate", CurrencyController.calculate);
 
 module.exports = router;
