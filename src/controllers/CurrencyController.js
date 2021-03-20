e27b081e40bd605abec40aebd820734c10f0a608
pessolatohenrique <pessolatohenrique@gmail.com>
2021-03-19 23:18:36 -0300

    Calculo de preco, com validacao via parametro

--- src/controllers/CurrencyController.js
+++ src/controllers/CurrencyController.js
@@ -6,15 +6,31 @@ class CurrencyController {
 
   static async calculate(req, res) {
     try {
+      const { price } = req.query;
+
+      const isNumeric = /^-?\d+$/.test(price);
+
+      if (!price || price <= 0 || !isNumeric) {
+        return res
+          .status(400)
+          .json({ message: "Price must be valid and above 0 " });
+      }
+
       const response = await axios.get(
         "https://api.exchangeratesapi.io/latest?base=BRL&symbols=USD,EUR,INR"
       );
 
       const valuesCalcutated = Object.keys(response.data.rates).map((key) => {
         const value = response.data.rates[key];
-        const resultCalculated = value < 1 ? 1 / value : 1 * value;
+        const singleValue = value < 1 ? 1 / value : 1 * value;
+        const convertedPrice = price * value;
 
-        return { code: key, result: resultCalculated.toFixed(2) };
+        return {
+          code: key,
+          comparative: value.toFixed(2),
+          single: singleValue.toFixed(2),
+          price: convertedPrice.toFixed(2),
+        };
       });
 
       return res.status(200).json(valuesCalcutated);
